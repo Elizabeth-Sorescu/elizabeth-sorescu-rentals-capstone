3fe6101d835b437f949c54c083d20302ad6aad3a
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-04-08 15:32:41 -0700

    create tables: properties, tenants, landlords

--- elizabeth-sorescu-rentals-server/migrations/20240408202006_create_properties_tables.js
+++ elizabeth-sorescu-rentals-server/migrations/20240408202006_create_properties_tables.js
@@ -2,10 +2,33 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("properties", (table) => {
+    table.increments("id").primary();
+    table.string("property_name", 255).notNullable();
+    table.string("street_address", 255).notNullable();
+    table.string("city", 255).notNullable();
+    table.string("country", 255).notNullable();
+    table.string("description", 255).notNullable();
+    table.string("type", 255).notNullable();
+    table.decimal("monthly_rent").notNullable();
+    table.decimal("rating").notNullable();
+    table.integer("num_reviews").notNullable();
+    table.decimal("mortgage").notNullable();
+    table.decimal("strata_fee").notNullable();
+    table.decimal("property_tax").notNullable();
+    table.decimal("maintenance_fee").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("properties");
+};
--- elizabeth-sorescu-rentals-server/migrations/20240408202132_create_tenants_tables.js
+++ elizabeth-sorescu-rentals-server/migrations/20240408202132_create_tenants_tables.js
@@ -2,14 +2,36 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("tenants", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("property_id")
+      .unsigned()
+      .references("properties.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.string("tenant_name", 255).notNullable();
+    table.string("room_location", 255).notNullable();
+    table.decimal("monthly_rent").notNullable();
+    table.string("messages", 255).notNullable();
+    table.decimal("rating").notNullable();
+    table.integer("num_reviews").notNullable();
+    table.string("email", 255).notNullable();
+    table.string("phone", 255).notNullable();
+    table.string("password", 255).notNullable();
+    table.string("role", 255).notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("tenants");
 };
