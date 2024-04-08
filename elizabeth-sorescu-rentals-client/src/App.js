1aa34f54298c3181038df1b6188a0fae8a3aa18d
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-04-08 12:11:44 -0700

    setup fe react router

--- elizabeth-sorescu-rentals-client/src/App.js
+++ elizabeth-sorescu-rentals-client/src/App.js
@@ -1,10 +1,31 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
     <>
-      <h1>Rentals Client</h1>
+      <BrowserRouter>
+        <Routes>
+          {/* Routes for welcomepage */}
+          {/* <Route path="/" element={<Welcome />} /> */}
+
+          {/* Route for a properties list page*/}
+          {/* <Route path="/properties" element={<Properties />} /> */}
+
+          {/* <Route
+            path="/properties/:id/tenants"
+            element={<PropertyDetails key={selectedProperty.id} />}
+          /> */}
+          {/* Route for add property page*/}
+          {/* <Route path="/properties/add" element={<AddProperty />} /> */}
+
+          {/* Route for tenant page */}
+          {/* <Route path="/tenant/*" element={<Tenant />} /> */}
+
+          {/* Route for other path */}
+          {/* <Route path="/*" element={<OtherRoutes />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
