ecaa723d1c91cdd26250835567054ee2d0ff4472
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-04-09 18:04:29 -0700

    dev-t7-fe-welcome-page

--- elizabeth-sorescu-rentals-client/src/App.js
+++ elizabeth-sorescu-rentals-client/src/App.js
@@ -1,31 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+// import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          {/* Routes for welcomepage */}
-          {/* <Route path="/" element={<Welcome />} /> */}
+      <h1>Rentals</h1>
+      {/* <BrowserRouter>
+        <Routes> */}
+      {/* Routes for welcomepage */}
+      {/* <Route path="/" element={<Welcome />} /> */}
 
-          {/* Route for a properties list page*/}
-          {/* <Route path="/properties" element={<Properties />} /> */}
+      {/* Route for a properties list page*/}
+      {/* <Route path="/properties" element={<Properties />} /> */}
 
-          {/* <Route
+      {/* <Route
             path="/properties/:id/tenants"
             element={<PropertyDetails key={selectedProperty.id} />}
           /> */}
-          {/* Route for add property page*/}
-          {/* <Route path="/properties/add" element={<AddProperty />} /> */}
+      {/* Route for add property page*/}
+      {/* <Route path="/properties/add" element={<AddProperty />} /> */}
 
-          {/* Route for tenant page */}
-          {/* <Route path="/tenant/*" element={<Tenant />} /> */}
+      {/* Route for tenant page */}
+      {/* <Route path="/tenant/*" element={<Tenant />} /> */}
 
-          {/* Route for other path */}
-          {/* <Route path="/*" element={<OtherRoutes />} /> */}
-        </Routes>
-      </BrowserRouter>
+      {/* Route for other path */}
+      {/* <Route path="/*" element={<OtherRoutes />} /> */}
+      {/* </Routes>
+      </BrowserRouter> */}
     </>
   );
 }
---
+++ elizabeth-sorescu-rentals-client/src/components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+// import logo from "../../assets/logos/rentals-logo.png";
+// import { NavLink } from "react-router-dom";
+import "../Welcome/Welcome.scss";
+
+function Welcome() {
+  return (
+    <div>
+      {/* <h1>Rentals</h1> */}
+
+      {/* <img className="rentals-logo" src={logo} alt="rentals Logo"></img> */}
+    </div>
+  );
+}
+
+export default Welcome;
