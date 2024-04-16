a09daa2b1e00657f0c84cb3f643126f36d1816ad
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-04-16 14:20:12 -0700

    fix header styling

--- elizabeth-sorescu-rentals-client/src/components/Header/Header.js
+++ elizabeth-sorescu-rentals-client/src/components/Header/Header.js
@@ -7,68 +7,25 @@ import { NavLink, Link } from "react-router-dom";
 function Header() {
   return (
     <nav className="header-nav">
-      <label className="header-nav__logo">
+      <label>
         <NavLink to="/">
-          <div className="logo">
-            {/* RenTaL$ */}
-            <img className="logo__pic" src={logo} alt="rentals Logo" />
+          <div className="header-nav__logo">
+            <img src={logo} alt="rentals logo" />
           </div>
         </NavLink>
       </label>
       <div className="header-nav__menu">
-        <Link to="/current/user">
-          <img
-            className="icon__item--pic"
-            src={profileIcon}
-            alt="user profile icon"
-          ></img>
-        </Link>
-        <Link to="/properties">
-          <img
-            className="icon__item--pic"
-            src={homeIcon}
-            alt="user profile icon"
-          ></img>
-        </Link>
+        <div className="header-nav__menu--pic">
+          <Link to="/current/user">
+            <img src={profileIcon} alt="user profile icon"></img>
+          </Link>
+        </div>
+        <div className="header-nav__menu--pic">
+          <Link to="/properties">
+            <img src={homeIcon} alt="user profile icon"></img>
+          </Link>
+        </div>
       </div>
-
-      {/* <div>
-        <Link to="/current/user">
-          <img
-            className="icon__item--pic"
-            src={profileIcon}
-            alt="user profile icon"
-          ></img>
-        </Link>
-
-        <Link to="/properties">
-          <img
-            className="icon__item--pic"
-            src={homeIcon}
-            alt="user profile icon"
-          ></img>
-        </Link>
-      </div> */}
-      {/* 
-      <div className="icon">
-        <div className="icon__item">
-          <img
-            className="icon__item--pic"
-            src={profileIcon}
-            alt="user profile icon"
-          ></img>
-          <p className="icon__item--label">Profile</p>
-        </div>
-
-        <div className="icon__item">
-          <img
-            className="icon__item--pic"
-            src={homeIcon}
-            alt="user profile icon"
-          ></img>
-          <p className="icon__item--label">Home</p>
-        </div>
-      </div> */}
     </nav>
   );
 }
