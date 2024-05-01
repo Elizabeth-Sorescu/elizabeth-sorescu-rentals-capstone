3786625f0a4650ad9de5f207d078be9f57513461
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-04-30 19:22:23 -0700

    add style to ProfileInfo

--- elizabeth-sorescu-rentals-client/src/components/ProfileInfo/ProfileInfo.js
+++ elizabeth-sorescu-rentals-client/src/components/ProfileInfo/ProfileInfo.js
@@ -1,17 +1,36 @@
 import React from "react";
 import "../ProfileInfo/ProfileInfo.scss";
+import { Link } from "react-router-dom";
+import avatar from "../../assets/icons/avatar.svg";
+import payment from "../../assets/images/payment-details.svg";
+import settings from "../../assets/images/settings.svg";
+import services from "../../assets/images/contact-handyman.svg";
+import backBtn from "../../assets/icons/back-button.svg";
 
 function ProfileInfo({ user, handleLogout }) {
   return (
     <main className="profile-info">
       <section className="profile-info__elem">
-        <img src="" alt="user avatar"></img>
-        <h1>Hi, {user.name}!</h1>
+        <div className="profile__heading">
+          <Link to="/current/user">
+            <img
+              className="profile__heading--back-btn"
+              src={backBtn}
+              alt="back button"
+            ></img>
+          </Link>
+          <h1 className="profile__heading--label">Profile</h1>
+        </div>
+        <img src={avatar} alt="user avatar"></img>
+        <h1>Welcome, {user.name}!</h1>
         <p>Email: {user.email}</p>
         <p>Phone: {user.phone}</p>
-        <p>id: {user.id}</p>
-        <p>role: {user.role}</p>
-        <button className="profile__logout" onClick={handleLogout}>
+        <p>Role: {user.role}</p>
+        <img src={payment} alt="payment details" />
+        <img src={settings} alt="settings" />
+        <img src={services} alt="handyman services" />
+
+        <button className="profile-info__logout-btn" onClick={handleLogout}>
           Logout
         </button>
       </section>
